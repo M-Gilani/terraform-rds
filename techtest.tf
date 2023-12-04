9768d62127ae241c165e65574a96502d97f4ab3d
M-Gilani <152820827+M-Gilani@users.noreply.github.com>
2023-12-04 00:39:42 +0000

    Update techtest.tf

--- techtest.tf
+++ techtest.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "private_subnet_2" {
 }
 
 resource "aws_db_subnet_group" "example_db_subnet_group" {
-  name       = "example-db-subnet-group"  # Adjusted subnet group name
+  name       = "example-db-subnet-group"  
   subnet_ids = [
     aws_subnet.private_subnet_1.id,
     aws_subnet.private_subnet_2.id
